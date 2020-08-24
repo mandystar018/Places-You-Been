c3be137f7bfafb7bde2495c1e807cf7775aed454
Amanda Otero <mandystar018@gmail.com>
2020-08-24 09:42:03 -0700

    Add prototype.addPlace and prototype.assingId with functions
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,10 @@ function placesVisited(location, landmarks, timeOfYear, notes) {
 let mylocation = new placesVisited("Switzerland", "Schilltorn Mountain", "September, 2003", "Ate cheese and bread on top of the mountain");
 console.log (mylocation.location)
 
+
+
+
+
 $(document).ready(function(){
 
 });
