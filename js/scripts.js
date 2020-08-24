1754406470777a89a74314fa2ee51a574cbe101d
Amanda Otero <mandystar018@gmail.com>
2020-08-24 09:24:53 -0700

    Add variable mylocation test1
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //Business Logic
-function placesVisited( location, landmarks, timeOfYear, notes) {
+function placesVisited(location, landmarks, timeOfYear, notes) {
   this.location = location;
   this.landmarks = landmarks;
   this.timeOfYear = timeOfYear;
   this.notes = notes;
 }
 
-
-
+let mylocation = new placesVisited("Switzerland", "Schilltorn Mountain", "September, 2003", "Ate cheese and bread on top of the mountain");
+console.log (mylocation.location)
 
 $(document).ready(function(){
 
