fc4b739dd4fbb83f33cd5df1a424965136a3a622
Amanda Otero <mandystar018@gmail.com>
2020-08-24 09:07:35 -0700

    Add directories and files to the project and starting working with the constroctor function
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Business Logic
+function placesVisited( location, landmarks, timeOfYear, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+
+
+
+$(document).ready(function(){
+
+});
